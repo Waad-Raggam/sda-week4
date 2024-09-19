43a7d8943c982edc6ed06fb779edc68b0641fc8f
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-09-19 04:28:33 +0300

    Add swagger

--- Program.cs
+++ Program.cs
@@ -3,7 +3,18 @@ using ecommerce;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 // endpoint: http://localhost:5212
 app.MapGet("", () =>
 {
