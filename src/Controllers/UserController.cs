208a9d36326bfe1b6aeb1cc52b99691e6b314bfd
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-09-19 14:13:00 +0300

    Create UserController.cs

---
+++ src/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerce
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    // base endpoint: api/v1/users
+    class UserController : ControllerBase
+    {
+        
+    }
+}
