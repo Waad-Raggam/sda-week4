4a1b5c093c6601f06c3842d3754ee4f4a523a650
waadshaher <108654364+Waad-Raggam@users.noreply.github.com>
2024-09-26 14:00:41 +0300

    Add db constructor

--- src/Database/DatabaseContext.cs
+++ src/Database/DatabaseContext.cs
@@ -5,6 +5,14 @@ namespace ecommerce
 {
     class DatabaseContext : DbContext
     {
+        // erd
+        // category
         public DbSet<Category> Category {get; set;}
+        // public DbSet<Product> Product {get; set;}
+
+        // constructor is for db configuration
+        public DatabaseContext(DbContextOptions options) : base(options) {
+
+        }
     }
 }
